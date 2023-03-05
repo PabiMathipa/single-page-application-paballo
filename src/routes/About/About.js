829004c694452c2dc77bee1546e3cb9c76c69b7b
Paballo Mathipa <pmathipa021@student.wethinkcode.co.za>
2023-03-05 15:24:23 +0200

    changed about

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -16,18 +16,7 @@ function About() {
                 problem-solving, and my desire to learn quickly adapts me to changing situations.
 
            </p>
-           <h2>Why Sovtech</h2>
-            <p>
-                As one of the leading custom software development company in South Africa,
-                backed by multiple industry awards, I believe being part of you programme will
-                help me gain experience and grow my confidence in programming. I am impressed with
-                the company's culture and work ethics, and I feel I would be a perfect fit to the company.
-                Sovtech has strong company values and I emulate with all of them, especially <em>#</em>Adapt
-                because technology has evolved over the years and it is important for a person in technology
-                to adjust to change happening within the industry.
-
-
-            </p>
+           
         </section>
 
     )
